4fe274408ed4e7cfeac0f9c31b602f40008702a3
Gabriel Freytag <gabrielfrtg@gmail.com>
2023-08-08 20:01:55 -0300

    Removes hardcoded NVIDIA SM version

--- original/CUDA/flags.mk
+++ original/CUDA/flags.mk
@@ -1,5 +1,5 @@
 CC=$(GCC)
 CFLAGS=-O3
 GPUCC=$(NVCC)
-GPUCFLAGS=-Xptxas="-v" --maxrregcount 127 --gpu-architecture sm_75 # $(CUDA_GPU_SM)
+GPUCFLAGS=-Xptxas="-v" --maxrregcount 127 --gpu-architecture $(CUDA_GPU_SM)
 LIBS=-L/usr/local/cuda/lib64 -lcudart -lstdc++ $(GCC_LIBS)

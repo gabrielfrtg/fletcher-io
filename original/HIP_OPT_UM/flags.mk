cfe3805e1005564b2ddf2d9f50fcc609778609be
Cristiano Alex Künas <cakunas@inf.ufrgs.br>
2026-01-27 14:22:44 -0300

    Update GPU architecture to gfx1100 for Radeon RX 7900 XT (RDNA 3)
    - Set default GPU_ARCH to gfx1100
    - Made GPU_ARCH configurable via make variable
    - Added supported GPU options:
      * gfx1100 - Radeon RX 7900 XT/XTX (RDNA 3)
      * gfx942  - MI300X (CDNA 3)
      * gfx90a  - MI250/MI250X (CDNA 2)
      * gfx908  - MI100 (CDNA 1)
      * gfx906  - MI50/MI60

--- original/HIP_OPT_UM/flags.mk
+++ original/HIP_OPT_UM/flags.mk
@@ -1,7 +1,14 @@
 CC=gcc
 PGCC=hipcc
 CFLAGS=-lm
-PGCCFLAGS=-O3 -x hip -D__HIP_ROCclr__ -D__HIP_ARCH_GFX90A__=1 -D__HIP_PLATFORM_AMD__ --rocm-path=${ROCM_PATH} --offload-arch=gfx90a
+# GPU Architecture: gfx1100 = Radeon RX 7900 XT/XTX (RDNA 3)
+# Other options:
+#   gfx942  = MI300X (CDNA 3)
+#   gfx90a  = MI250/MI250X (CDNA 2)
+#   gfx908  = MI100 (CDNA 1)
+#   gfx906  = MI50/MI60
+GPU_ARCH ?= gfx1100
+PGCCFLAGS=-O3 -x hip -D__HIP_ROCclr__ -D__HIP_ARCH_GFX1100__=1 -D__HIP_PLATFORM_AMD__ --rocm-path=${ROCM_PATH} --offload-arch=$(GPU_ARCH)
 
 LIBS =
 
